89ca8730045fa541bf121b4ecd1593a003c0c9cb
xmhtoolathlon <ygytyygtftytdrfcvgfrteseswaqqw@gmail.com>
2026-02-08 14:36:10 +0800

    Add neptune/analytics/aggregator.py with FIXME comments

---
+++ neptune/analytics/aggregator.py
@@ -0,0 +1,16 @@
+"""Data aggregation module"""
+
+class Aggregator:
+    # FIXME: Add window function support
+    # FIXME: Implement rolling aggregations
+    
+    def group_by(self, data, keys):
+        # FIXME: Optimize grouping for large datasets
+        # FIXME: Add null handling strategy
+        pass
+    
+    def compute_stats(self, data, columns):
+        # FIXME: Add percentile calculations
+        # FIXME: Implement variance and standard deviation
+        # FIXME: Support weighted statistics
+        pass
